ab7d68871b9e18fd110a98eafd0f597cc32a8649
Luca Boccardo <luca.boccardo@kcl.ac.uk>
2022-11-14 19:13:18 +0000

    Complete Exercise 2.1

---
+++ things/models.py
@@ -0,0 +1,26 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.db import models
+
+# Create your models here.
+
+
+class Thing:
+    name = models.CharField(
+        blank=False,
+        max_length=30,
+        unique=True,
+    )
+
+    description = models.CharField(
+        blank=False,
+        max_length=120,
+        unique=False,
+    )
+
+    quantity = models.IntegerField(
+        unique=False,
+        validators=[
+            MinLengthValidator(0, message="Please enter a number between 0 and 100"),
+            MaxLengthValidator(100, message="Please enter a number between 0 and 100"),
+        ],
+    )
